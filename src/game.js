524833267b501cf8322671c16d0243e9b97aa165
Rahul <yveltal4286@gmail.com>
2024-08-18 23:18:30 +0530

    feat: display question & choices & checked pseudo-class used

--- src/game.js
+++ src/game.js
@@ -1,3 +1,4 @@
+// Promise function to fetch quiz from the opentdb-api
 async function fetchQuiz() {
   const quiz = await fetch(
     "https://opentdb.com/api.php?amount=12&category=22&difficulty=easy&type=multiple"
@@ -8,10 +9,12 @@ async function fetchQuiz() {
   return quiz;
 }
 
+// Promise function to extract the questions & answer choices from the quiz-api to make a new object { questions, answers }
 async function getData() {
   const quiz = await fetchQuiz();
   let questions = [],
-    answers = [];
+    answers = [],
+    correct = [];
   for (let qnIndex = 0; qnIndex < quiz.length; qnIndex++) {
     const qns = quiz[qnIndex];
     const qstn = qns.question;
@@ -22,6 +25,7 @@ async function getData() {
     const qns = quiz[qnIndex];
     const crct = qns.correct_answer;
     const incrct = qns.incorrect_answers;
+    correct.push(crct);
     choices.push(crct);
     for (let i = 0; i < incrct.length; i++) {
       choices.push(incrct[i]);
@@ -29,10 +33,22 @@ async function getData() {
     choices = Object.assign({}, choices);
     answers.push(choices);
   }
-  return { questions, answers };
+  return { questions, answers, correct };
 }
 
 getData().then(
-  (res) => console.log(res),
+  (res) => {
+    let question = document.querySelector("#game-question");
+    const choice = document.querySelectorAll(".choices-text");
+    question.innerHTML = res.questions[0];
+    let i = 0;
+    let answerObj = res.answers[0];
+    while (i < 4) {
+      choice.forEach((e) => {
+        e.innerHTML = answerObj[i];
+        i++;
+      });
+    }
+  },
   (err) => console.log(err)
 );
