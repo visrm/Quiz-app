c4686a72992e3814ef8351d9484c5a35dbc7c507
Rahul <yveltal4286@gmail.com>
2024-11-02 19:08:24 +0530

    :heavy_plus_sign: new radio button & answer evaluation method

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -9,7 +9,8 @@ async function fetchQuiz() {
   return quiz;
 }
 
-// Promise function to extract the questions & answer choices from the quiz-api to make a new object { questions, answers }
+// Promise function to extract the questions & answer choices
+// from the quiz-api to make a new object { questions, answers, correct }
 async function getData() {
   const quiz = await fetchQuiz();
   var questions = [],
@@ -38,24 +39,35 @@ async function getData() {
     choices = Object.assign({}, choices);
     answers.push(choices); // update "answers" array.
   }
+
   return { questions, answers, correct };
 }
 
-getData().then(
+const data = getData();
+data.then(
   (res) => {
     // CONSTANTS
     const CORRECT_BONUS = 5;
     const MAX_QUESTIONS = 12;
 
+    let selectedChoice = [];
     let i = 0,
       score = 0;
     let question = document.querySelector("#game-question");
     const game = document.querySelector("#game");
+    const radios = document.querySelectorAll(".choice-radio");
     const choices = document.querySelectorAll(".choices-text");
+    const radioChoices = document.getElementsByName("question-choice");
     const submit = document.querySelector(".submit-btn");
 
     question.innerHTML = `${i + 1}. ${res.questions[0]}`;
     let answerObj = res.answers[0];
+    for (let j = 0; j < 4; j++) {
+      radios.forEach((radio) => {
+        radio.value = answerObj[j];
+        j++;
+      });
+    }
     for (let j = 0; j < 4; j++) {
       choices.forEach((choice) => {
         choice.innerHTML = answerObj[j];
@@ -63,13 +75,42 @@ getData().then(
       });
     }
 
+    radioChoices.length > 0 &&
+      radioChoices.forEach((radioChoice) => {
+        if (radioChoice.checked) selectedChoice.push(radioChoice.value);
+        radioChoice.addEventListener("click", () => {
+          if (radioChoice.checked) {
+            selectedChoice.push(radioChoice.value);
+          }
+        });
+      });
+
     // To prevent page-refresh on submit of each answer choice.
     game.addEventListener("submit", (event) => {
       event.preventDefault();
-      console.log(event.target.value);
-    });
 
-    submit.addEventListener("click", () => {
+      if (selectedChoice && selectedChoice.length >= 0) {
+        if (selectedChoice.length == 0)
+          selectedChoice.push(radioChoices[0].value); // set default value of default selected radio button
+        let chosenSelection =
+          selectedChoice[selectedChoice.length - 1].toString();
+        if (chosenSelection == res.correct[i]) {
+          score += CORRECT_BONUS;
+          // for test purpose :
+          // console.info(`Correct Answer! Your score is now : ${score}`);
+        } else {
+          // for test purpose :
+          // console.info(
+          //   `Your choice: ${chosenSelection}, The Correct Answer is: ${res.correct[i]}`
+          // );
+          score += 0;
+        }
+      } else {
+        window.alert("Select your answer");
+      }
+
+      selectedChoice.splice(0, selectedChoice.length);
+
       if (i < MAX_QUESTIONS - 1) {
         i++;
         do {
@@ -81,10 +122,19 @@ getData().then(
               j++;
             });
           }
+          for (let j = 0; j < 4; j++) {
+            radios.forEach((radio) => {
+              radio.value = answerObj[j];
+              j++;
+            });
+          }
         } while (i < 1);
-      }
+      } else return;
+    });
 
-      // if()
+    submit.addEventListener("click", () => {
+      var defaultRadioBtn = document.getElementById("ch1");
+      defaultRadioBtn.checked = true; // set default radio button to "checked" state after submit
     });
   },
   (err) => console.log(err)
